7c48fc984949db8c8e13ce7e8e7a1d62b3e4a32f
Brian Chung <bchung1@umd.edu>
2016-08-17 18:45:53 -0400

    added wedj project

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
 	$('#summarizer').hover(function(){
 		$('.project-name').html("SUMMERy");
 		$('#project-description').html("SUMMERy is a google chrome extension that summarizes online news articles. This was my submission for Capital One's Software Engineering Summit Challenge 2016. It was selected as one of the top 30 submissions.");
-		$('#project-description').css({'color':'#BA002E'});
 		$('.center-icon > a > i').show(); 
 		$('.center-icon > a').attr('href','https://github.com/bchung1/Capital-One-Challenge');
 	});
@@ -11,7 +10,6 @@ $(document).ready(function(){
 	$('#proximity-print').hover(function(){
 		$('.project-name').html('Proximity Print');
 		$('#project-description').html("Proximity Print is a printing service that will allow students to request to print from printers around them.");
-		$('#project-description').css({'color':'#BA002E'});
 		$('.center-icon > a > i').show(); 
 		$('.center-icon > a').attr('href','https://github.com/WHSnyder/print');
 	});
@@ -19,7 +17,6 @@ $(document).ready(function(){
 	$('#catalyst').hover(function(){
 		$('.project-name').html('Catalyst');
 		$('#project-description').html("Catalyst is a web app that helps young and naive college students (like myself) manage their financials."); 
-		$('#project-description').css({'color':'#BA002E'});
 		$('.center-icon > a > i').show(); 
 		$('.center-icon > a').attr('href','https://github.com/k--chow/Catalyst');
 	});
@@ -27,7 +24,6 @@ $(document).ready(function(){
 	$('#my-site').hover(function(){
 		$('.project-name').html('My Website');
 		$("#project-description").html("Description is self-explanatory. Check it out if you're interested!"); 
-		$('#project-description').css({'color':'#BA002E'});
 		$('.center-icon > a > i').show(); 
 		$('.center-icon > a').attr('href','https://github.com/bchung1/Personal-Website');
 	});
@@ -35,9 +31,15 @@ $(document).ready(function(){
 	$('#wiki-preview').hover(function(){ 
 		$('.project-name').html('Wiki-Preview');
 		$("#project-description").html("Wiki-Preview is a google chrome extension that gives you a preview of a wiki page as a tooltip. I made this extension because I was tired of opening dozens of wikipedia tabs :D"); 
-		$('#project-description').css({'color':'#BA002E'});
 		$('.center-icon > a > i').show(); 
 		$('.center-icon > a').attr('href','https://github.com/bchung1/wiki-preview');
+	}); 
+
+	$('#wedj').hover(function(){ 
+		$('.project-name').html('Wedj');
+		$("#project-description").html("Wedj or \"We DJ\" is a web application that creates a collaborative music experience. It allows the public to decide what music they want to hear in a shared space be it a gym, starbucks, or a party."); 
+		$('.center-icon > a > i').show(); 
+		$('.center-icon > a').attr('href','https://github.com/bchung1/wedj');
 	}); 
 
 	$('.tbd-project').hover(function(){
@@ -57,8 +59,17 @@ $(document).ready(function(){
 	$('#catalyst').click(function(){ 
 		window.location = 'https://github.com/k--chow/Catalyst';
 	}); 
+
 	$('#my-site').click(function(){ 
 		window.location = 'https://github.com/bchung1/Personal-Website';
+	}); 
+
+	$('#wiki-preview').click(function(){ 
+		window.location = 'https://github.com/bchung1/wiki-preview';
+	}); 
+
+	$('#wedj').click(function(){ 
+		window.location = 'https://github.com/bchung1/wedj';
 	}); 
 
 	$('#right-bracket').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){ 
